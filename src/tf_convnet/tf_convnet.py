3e4a82ee253eff8f1d6d03a0e6e2b47a453ebd82
cbreider <christoan.breiderhoff@googlemail.com>
2019-12-20 13:56:58 +0100

    fix typo

--- src/tf_convnet/tf_convnet.py
+++ src/tf_convnet/tf_convnet.py
@@ -106,7 +106,7 @@ class ConvNetModel(object):
                     self.correct_pred = tf.equal(self.pred_slice, self.y_slice)
                 else:
                     self.predicter = tf.cast(tf.nn.sigmoid(self.logits), tf.int32)
-                    self.correct_pred = tf.equal(self.predicter, tf.cast(self.y_slice), tf.int32)
+                    self.correct_pred = tf.equal(self.predicter, tf.cast(self.y_slice, tf.int32))
                 self.accuracy = tf.reduce_mean(tf.cast(self.correct_pred, tf.float32))
                 self.error = tf.constant(1.0) - self.accuracy
                 self.error_rate = tf.math.multiply(self.error, tf.constant(100.0))
