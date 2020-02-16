bfc629389a46b3f93de5d3fe151f7f75430a517b
cbreider <christian.breiderhoff@googlemail.com>
2020-02-16 22:59:48 +0100

    Logging level to warning

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -18,7 +18,7 @@ def init_logger(type="train", path=""):
     log_formatter = logging.Formatter("%(asctime)s %(message)s")
     root_logger = logging.getLogger()
     root_logger.handlers = []
-    root_logger.setLevel(logging.DEBUG)
+    root_logger.setLevel(logging.WARNING)
     file_handler = logging.FileHandler("{0}/{1}.log".format(path, type))
     file_handler.setFormatter(log_formatter)
     root_logger.addHandler(file_handler)
