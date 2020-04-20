01be50ba9d24aa141a3cb9f99a29a58700266d19
cbreider <christian.breiderhoff@googlemail.com>
2020-04-21 01:23:25 +0200

    Revert "fix"
    This reverts commit 637eac91bfef6dd713ae9c4f0ed312cee78eec42.

--- scripts/pre_train_base_lines.sh
+++ scripts/pre_train_base_lines.sh
@@ -4,26 +4,26 @@
 
 #conda activate masterp36
 
-cp ../configs/configuration_tv_regression_single.py ../configuration.py
-python3 ../train.py --mode 1 --cuda_device 0 --name="Single_Scale" --create_new_split
+cp configs/configuration_tv_regression_single.py configuration.py
+python3 train.py --mode 1 --cuda_device 0 --name="Single_Scale" --create_new_split
 
-cp ../configs/configuration_tv_regression_multi.py ../configuration.py
-python3 ../train.py --mode 1 --cuda_device 0 --name="Multi_Scale"
+cp configs/configuration_tv_regression_multi.py configuration.py
+python3 train.py --mode 1 --cuda_device 0 --name="Multi_Scale"
 
-cp ../configs/configuration_tv_segmentation_single.py ../configuration.py
-python3 ../train.py --mode 2 --cuda_device 0 --name="Single_Scale" 
+cp configs/configuration_tv_segmentation_single.py configuration.py
+python3 train.py --mode 2 --cuda_device 0 --name="Single_Scale" 
 
-cp ../configs/configuration_tv_segmentation_multi.py ../configuration.py
-python3 ../train.py --mode 2 --cuda_device 0 --name="Multi_Scale"
+cp configs/configuration_tv_segmentation_multi.py configuration.py
+python3 train.py --mode 2 --cuda_device 0 --name="Multi_Scale"
 
-cp ../configs/configuration_autoencoder.py ../configuration.py
-python3 ../train.py --mode 5 --cuda_device 0 --name="Skip_Layers"
+cp configs/configuration_autoencoder.py configuration.py
+python3 train.py --mode 5 --cuda_device 0 --name="Skip_Layers"
 
-cp ../configs/configuration_autoencoder.py ../configuration.py
-python3 ../train.py --mode 4 --cuda_device 0 --name="Skip_Layers"
+cp configs/configuration_autoencoder.py configuration.py
+python3 train.py --mode 4 --cuda_device 0 --name="Skip_Layers"
 
-cp ../configs/configuration_autoencoder_wo_skip_layers.py ../configuration.py
-python3 ../train.py --mode 4 --cuda_device 0 --name="No_Skip_Layers"
+cp configs/configuration_autoencoder_wo_skip_layers.py configuration.py
+python3 train.py --mode 4 --cuda_device 0 --name="No_Skip_Layers"
 
 
 
--- scripts/run_test_scratch.sh
+++ scripts/run_test_scratch.sh
@@ -4,26 +4,26 @@
 
 #conda activate masterp36
 
-python3 ../train.py --mode 3 --cuda_device 0 --nr_training_scans 1.0 --include_testing --create_new_split
+python3 train.py --mode 3 --cuda_device 0 --nr_training_scans 1.0 --include_testing --create_new_split
 
-python3 ../train.py --mode 3 --cuda_device 0 --nr_training_scans 0.9 --include_testing
+python3 train.py --mode 3 --cuda_device 0 --nr_training_scans 0.9 --include_testing
 
-python3 ../train.py --mode 3 --cuda_device 0 --nr_training_scans 0.8 --include_testing
+python3 train.py --mode 3 --cuda_device 0 --nr_training_scans 0.8 --include_testing
 
-python3 ../train.py --mode 3 --cuda_device 0 --nr_training_scans 0.7 --include_testing
+python3 train.py --mode 3 --cuda_device 0 --nr_training_scans 0.7 --include_testing
 
-python3 ../train.py --mode 3 --cuda_device 0 --nr_training_scans 0.6 --include_testing
+python3 train.py --mode 3 --cuda_device 0 --nr_training_scans 0.6 --include_testing
 
-python3 ../train.py --mode 3 --cuda_device 0 --nr_training_scans 0.5 --include_testing
+python3 train.py --mode 3 --cuda_device 0 --nr_training_scans 0.5 --include_testing
 
-python3 ../train.py --mode 3 --cuda_device 0 --nr_training_scans 0.4 --include_testing
+python3 train.py --mode 3 --cuda_device 0 --nr_training_scans 0.4 --include_testing
 
-python3 ../train.py --mode 3 --cuda_device 0 --nr_training_scans 0.3 --include_testing
+python3 train.py --mode 3 --cuda_device 0 --nr_training_scans 0.3 --include_testing
 
-python3 ../train.py --mode 3 --cuda_device 0 --nr_training_scans 0.2 --include_testing
+python3 train.py --mode 3 --cuda_device 0 --nr_training_scans 0.2 --include_testing
 
-python3 ../train.py --mode 3 --cuda_device 0 --nr_training_scans 0.15 --include_testing
+python3 train.py --mode 3 --cuda_device 0 --nr_training_scans 0.15 --include_testing
 
-python3 ../train.py --mode 3 --cuda_device 0 --nr_training_scans 0.1 --include_testing
+python3 train.py --mode 3 --cuda_device 0 --nr_training_scans 0.1 --include_testing
 
-python3 ../train.py --mode 3 --cuda_device 0 --nr_training_scans 0.05 --include_testing
+python3 train.py --mode 3 --cuda_device 0 --nr_training_scans 0.05 --include_testing
